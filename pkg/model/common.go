1dc14d0cb78804894a48661ca4576238410031b9
zhanghan <zhanghan@kylinos.cn>
2021-12-31 13:42:51 +0800

    add user's crud

--- pkg/model/common.go
+++ pkg/model/common.go
@@ -1,5 +1,11 @@
 package model
 
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	"openeluer.org/PilotGo/PilotGo/pkg/common/response"
+)
+
 /**
  * @Author: zhang han
  * @Date: 2021/11/12 17:01
@@ -18,15 +24,51 @@ type PageResult struct {
 	PageSize int         `json:"pageSize"`
 }
 
-type PaginationQ struct { // 定义分页
-	Ok    bool        `json:"ok"`
-	Size  uint        `form:"size" json:"size"`
-	Page  uint        `form:"page" json:"page"`
-	Data  interface{} `json:"data" comment:"muster be a pointer of slice gorm.Model"` // save pagination list
-	Total uint        `json:"total"`
+//分页查询结构体
+type PaginationQ struct {
+	Ok             bool        `json:"ok"`
+	Size           uint        `form:"size" json:"size"`
+	CurrentPageNum uint        `form:"page" json:"page"`
+	Data           interface{} `json:"data" comment:"muster be a pointer of slice gorm.Model"` // save pagination list
+	TotalPage      uint        `json:"total"`
 }
 
-type UserQ struct {
-	User
-	PaginationQ
+// 分页查询方法
+func crudAll(p *PaginationQ, queryTx *gorm.DB, list interface{}) (uint, error) {
+	if p.Size < 1 {
+		p.Size = 10
+	}
+	if p.CurrentPageNum < 1 {
+		p.CurrentPageNum = 1
+	}
+
+	var total uint
+	err := queryTx.Count(&total).Error
+	if err != nil {
+		return 0, err
+	}
+	offset := p.Size * (p.CurrentPageNum - 1)
+	err = queryTx.Limit(p.Size).Offset(offset).Find(list).Error
+	if err != nil {
+		return 0, err
+	}
+	return total, err
+}
+
+// 拼装json 分页数据
+func JsonPagination(c *gin.Context, list interface{}, total uint, query *PaginationQ) {
+	c.AbortWithStatusJSON(200, gin.H{
+		"ok":    true,
+		"data":  list,
+		"total": total,
+		"page":  query.CurrentPageNum,
+		"size":  query.Size})
+}
+
+func HandleError(c *gin.Context, err error) bool {
+	if err != nil {
+		response.Fail(c, gin.H{"ok": false}, "获取失败!")
+		return true
+	}
+	return false
 }
--- pkg/model/user.go
+++ pkg/model/user.go
@@ -8,6 +8,9 @@ package model
 
 import (
 	"github.com/jinzhu/gorm"
+	"github.com/sirupsen/logrus"
+	"golang.org/x/crypto/bcrypt"
+	"openeluer.org/PilotGo/PilotGo/pkg/mysqlmanager"
 )
 
 type User struct {
@@ -17,4 +20,32 @@ type User struct {
 	Phone    string `gorm:"size:255" json:"phone,omitempty" form:"phone"`
 	Email    string `gorm:"type:varchar(30);not null" json:"email,omitempty" form:"email"`
 	Enable   string `gorm:"size:10;not null" json:"enable,omitempty"`
+}
+
+type UserQ struct {
+	User
+	PaginationQ
+}
+
+func (u *User) All(q *PaginationQ) (list *[]User, total uint, err error) {
+	list = &[]User{}
+	tx := mysqlmanager.DB.Find(list)
+	total, err = crudAll(q, tx, list)
+	return
+}
+
+//Update
+func (m *User) Update() (err error) {
+	m.makePassword()
+	return mysqlmanager.DB.Model(m).Update(m).Error
+}
+
+func (m *User) makePassword() {
+	if m.Password != "" {
+		if bytes, err := bcrypt.GenerateFromPassword([]byte(m.Password), bcrypt.DefaultCost); err != nil {
+			logrus.WithError(err).Error("bcrypt making password is failed")
+		} else {
+			m.Password = string(bytes)
+		}
+	}
 }
