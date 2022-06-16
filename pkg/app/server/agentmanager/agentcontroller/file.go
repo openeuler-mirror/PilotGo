eb8b892ab5041d9a179930f25db7a13395b0efb1
zhanghan <zhanghan@kylinos.cn>
2022-06-16 17:11:29 +0800

    Rewrite configuration management module CRUD

--- pkg/app/server/agentmanager/agentcontroller/file.go
+++ pkg/app/server/agentmanager/agentcontroller/file.go
@@ -16,16 +16,10 @@
 package agentcontroller
 
 import (
-	"net/http"
-	"strconv"
-	"strings"
-
 	"github.com/gin-gonic/gin"
 	"openeluer.org/PilotGo/PilotGo/pkg/app/server/agentmanager"
 	"openeluer.org/PilotGo/PilotGo/pkg/app/server/controller"
-	"openeluer.org/PilotGo/PilotGo/pkg/app/server/dao"
 	"openeluer.org/PilotGo/PilotGo/pkg/app/server/model"
-	"openeluer.org/PilotGo/PilotGo/pkg/app/server/service"
 	"openeluer.org/PilotGo/PilotGo/pkg/utils/response"
 )
 
@@ -87,366 +81,149 @@ func GetAgentFiles(c *gin.Context) {
 	controller.JsonPagination(c, data, len(datas), query)
 }
 
-func SaveFileToDatabase(c *gin.Context) {
-	var file model.Files
-	c.Bind(&file)
-
-	path := file.SourcePath
-	filename := file.FileName
-	if len(filename) == 0 {
-		response.Fail(c, nil, "请输入配置文件名字")
-		return
-	}
-	description := file.Description
-	if len(description) == 0 {
-		response.Fail(c, nil, "请添加文件描述")
-		return
-	}
-	text := file.File
-	if len(text) == 0 {
-		response.Fail(c, nil, "请重新检查文件内容")
-		return
-	}
-
-	fd := model.Files{
-		SourcePath:  path,
-		FileName:    filename,
-		Description: description,
-		File:        text,
-	}
-	dao.SaveFile(fd)
-	response.Success(c, nil, "文件保存成功")
-}
-
 func UpdateAgentFile(c *gin.Context) {
-	var file model.HistoryFiles
-	c.Bind(&file)
+	// var file model.HistoryFiles
+	// c.Bind(&file)
 
-	ip := file.IP
-	uuid := file.UUID
-	path := file.Path
-	ipdept := file.IPDept
-	if len(path) == 0 {
-		response.Fail(c, nil, "请检查配置文件路径")
-		return
-	}
-	filename := file.FileName
-	if len(filename) == 0 {
-		response.Fail(c, nil, "请检查配置文件名字")
-		return
-	}
-	text := file.File
-	if len(text) == 0 {
-		response.Fail(c, nil, "请重新检查文件内容")
-		return
-	}
+	// ip := file.IP
+	// uuid := file.UUID
+	// path := file.Path
+	// ipdept := file.IPDept
+	// if len(path) == 0 {
+	// 	response.Fail(c, nil, "请检查配置文件路径")
+	// 	return
+	// }
+	// filename := file.FileName
+	// if len(filename) == 0 {
+	// 	response.Fail(c, nil, "请检查配置文件名字")
+	// 	return
+	// }
+	// text := file.File
+	// if len(text) == 0 {
+	// 	response.Fail(c, nil, "请重新检查文件内容")
+	// 	return
+	// }
 
-	agent := agentmanager.GetAgent(uuid)
-	if agent == nil {
-		response.Fail(c, nil, "server端获取uuid失败!")
-		return
-	}
+	// agent := agentmanager.GetAgent(uuid)
+	// if agent == nil {
+	// 	response.Fail(c, nil, "server端获取uuid失败!")
+	// 	return
+	// }
 
-	result, Err, err := agent.UpdateFile(path, filename, text)
-	if len(Err) != 0 || err != nil {
-		response.Fail(c, nil, Err)
-		return
-	}
+	// result, Err, err := agent.UpdateFile(path, filename, text)
+	// if len(Err) != 0 || err != nil {
+	// 	response.Fail(c, nil, Err)
+	// 	return
+	// }
 
-	if ok, id := dao.IsFileLatest(filename, uuid); ok {
-		dao.DeleteLastFile(id)
-	}
+	// if ok, id := dao.IsFileLatest(filename, uuid); ok {
+	// 	dao.DeleteLastFile(id)
+	// }
 
-	// 保存历史版本
-	time := service.NowTime()
-	fd := model.HistoryFiles{
-		IP:       ip,
-		IPDept:   ipdept,
-		UUID:     uuid,
-		Path:     path,
-		FileName: filename + "-" + time,
-		File:     result.(string),
-	}
-	dao.SaveHistoryFile(fd)
+	// // 保存历史版本
+	// time := service.NowTime()
+	// fd := model.HistoryFiles{
+	// 	IP:       ip,
+	// 	IPDept:   ipdept,
+	// 	UUID:     uuid,
+	// 	Path:     path,
+	// 	FileName: filename + "-" + time,
+	// 	File:     result.(string),
+	// }
+	// dao.SaveHistoryFile(fd)
 
-	response.JSON(c, http.StatusOK, http.StatusOK, result, "配置文件已更新")
-}
-
-func AllFiles(c *gin.Context) {
-	query := &model.PaginationQ{}
-	err := c.ShouldBindQuery(query)
-	if err != nil {
-		response.Response(c, http.StatusOK, http.StatusBadRequest, gin.H{"status": false}, err.Error())
-		return
-	}
-
-	files := model.Files{}
-	list, tx := files.AllFiles(query)
-	if err != nil {
-		response.Fail(c, gin.H{"status": false}, err.Error())
-		return
-	}
-
-	total, err := controller.CrudAll(query, tx, list)
-	if err != nil {
-		response.Fail(c, gin.H{"status": false}, err.Error())
-		return
-	}
-	controller.JsonPagination(c, list, total, query)
-}
-
-func AllHistoryFiles(c *gin.Context) {
-	query := &model.PaginationQ{}
-	err := c.ShouldBindQuery(query)
-	if err != nil {
-		response.Response(c, http.StatusOK, http.StatusBadRequest, gin.H{"status": false}, err.Error())
-		return
-	}
-
-	files := model.HistoryFiles{}
-	list, tx := files.AllHistoryFiles(query)
-	if err != nil {
-		response.Fail(c, gin.H{"status": false}, err.Error())
-		return
-	}
-
-	total, err := controller.CrudAll(query, tx, list)
-	if err != nil {
-		response.Fail(c, gin.H{"status": false}, err.Error())
-		return
-	}
-	controller.JsonPagination(c, list, total, query)
-}
-
-func FileSearch(c *gin.Context) {
-	var file model.SearchFile
-	c.Bind(&file)
-	search := file.Search
-
-	query := &model.PaginationQ{}
-	err := c.ShouldBindQuery(query)
-	if err != nil {
-		response.Response(c, http.StatusOK, http.StatusBadRequest, gin.H{"status": false}, err.Error())
-		return
-	}
-
-	list, tx := file.FileSearch(query, search)
-
-	total, err := controller.CrudAll(query, tx, list)
-	if err != nil {
-		response.Response(c, http.StatusOK, http.StatusBadRequest, gin.H{"status": false}, err.Error())
-		return
-	}
-	controller.JsonPagination(c, list, total, query)
-}
-
-func LastFileSearch(c *gin.Context) {
-	var file model.SearchFile
-	c.Bind(&file)
-	search := file.Search
-
-	query := &model.PaginationQ{}
-	err := c.ShouldBindQuery(query)
-	if err != nil {
-		response.Response(c, http.StatusOK, http.StatusBadRequest, gin.H{"status": false}, err.Error())
-		return
-	}
-
-	list, tx := file.LastFileSearch(query, search)
-
-	total, err := controller.CrudAll(query, tx, list)
-	if err != nil {
-		response.Response(c, http.StatusOK, http.StatusBadRequest, gin.H{"status": false}, err.Error())
-		return
-	}
-	controller.JsonPagination(c, list, total, query)
-}
-
-func UpdateFile(c *gin.Context) {
-	var file model.Files
-	c.Bind(&file)
-
-	path := file.SourcePath
-	filename := file.FileName
-	description := file.Description
-	text := file.File
-	if !dao.IsExistId(file.ID) {
-		response.Fail(c, nil, "id有误,请重新确认该文件是否存在")
-		return
-	}
-	dao.UpdateFile(file.ID, path, filename, description, text)
-
-	response.Success(c, nil, "配置文件修改成功")
-}
-
-func DeleteFile(c *gin.Context) {
-	var files model.DeleteFiles
-	c.Bind(&files)
-
-	for _, fileId := range files.FileIDs {
-		dao.DeleteFile(fileId)
-	}
-	response.Success(c, nil, "储存的文件已从数据库中删除")
-}
-
-func FindLastVersionFile(c *gin.Context) {
-	uuid := c.Query("uuid")
-	filename := c.Query("name")
-	lastfiles := dao.FindLastVersionFile(uuid, filename)
-	if lastfiles == nil {
-		response.Success(c, gin.H{"oldfiles": []interface{}{}}, "获取该文件的历史版本")
-		return
-	}
-	response.Success(c, gin.H{"oldfiles": lastfiles}, "获取该文件的历史版本")
-}
-
-func LastFileRollBack(c *gin.Context) {
-	var file model.HistoryFiles
-	c.Bind(&file)
-
-	ip := file.IP
-	uuid := file.UUID
-	path := file.Path
-	ipdept := file.IPDept
-	if len(path) == 0 {
-		response.Fail(c, nil, "请检查配置文件路径")
-		return
-	}
-	text := file.File
-	if len(text) == 0 {
-		response.Fail(c, nil, "请重新检查文件内容")
-		return
-	}
-
-	agent := agentmanager.GetAgent(uuid)
-	if agent == nil {
-		response.Fail(c, nil, "server端获取uuid失败!")
-		return
-	}
-
-	filename := file.FileName
-	if len(filename) == 0 {
-		response.Fail(c, nil, "请检查配置文件名字")
-		return
-	}
-	name := strings.Split(filename, "-")[0]
-	if ok, _ := dao.IsFileLatest(name, uuid); !ok {
-		LatestVersion, Err, err := agent.UpdateFile(path, name, text)
-		if len(Err) != 0 || err != nil {
-			response.Fail(c, nil, Err)
-			return
-		}
-		//  保存最新版本
-		fd := model.HistoryFiles{
-			IP:       ip,
-			IPDept:   ipdept,
-			UUID:     uuid,
-			Path:     path,
-			FileName: filename + "-latest",
-			File:     LatestVersion.(string),
-		}
-		dao.SaveHistoryFile(fd)
-		response.JSON(c, http.StatusOK, http.StatusOK, LatestVersion, "已回退到历史版本")
-		return
-	}
-
-	_, Err, err := agent.UpdateFile(path, name, text)
-	if len(Err) != 0 || err != nil {
-		response.Fail(c, nil, Err)
-		return
-	}
-	response.JSON(c, http.StatusOK, http.StatusOK, nil, "已回退到历史版本")
+	// response.JSON(c, http.StatusOK, http.StatusOK, result, "配置文件已更新")
 }
 
 func FileBroadcastToAgents(c *gin.Context) {
-	var fb model.FileBroadcast
-	c.Bind(&fb)
+	// var fb model.FileBroadcast
+	// c.Bind(&fb)
 
-	path := fb.Path
-	filename := fb.FileName
-	text := fb.Text
+	// path := fb.Path
+	// filename := fb.FileName
+	// text := fb.Text
 
-	if len(path) == 0 {
-		response.Fail(c, nil, "路径为空，请检查配置文件路径")
-		return
-	}
-	if len(filename) == 0 {
-		response.Fail(c, nil, "文件名为空，请检查配置文件名字")
-		return
-	}
-	if len(text) == 0 {
-		response.Fail(c, nil, "文件内容为空，请重新检查文件内容")
-		return
-	}
+	// if len(path) == 0 {
+	// 	response.Fail(c, nil, "路径为空，请检查配置文件路径")
+	// 	return
+	// }
+	// if len(filename) == 0 {
+	// 	response.Fail(c, nil, "文件名为空，请检查配置文件名字")
+	// 	return
+	// }
+	// if len(text) == 0 {
+	// 	response.Fail(c, nil, "文件内容为空，请重新检查文件内容")
+	// 	return
+	// }
 
-	logParent := model.AgentLogParent{
-		UserName:   fb.User,
-		DepartName: fb.UserDept,
-		Type:       model.LogTypeBroadcast,
-	}
-	logParentId := dao.ParentAgentLog(logParent)
+	// logParent := model.AgentLogParent{
+	// 	UserName:   fb.User,
+	// 	DepartName: fb.UserDept,
+	// 	Type:       model.LogTypeBroadcast,
+	// }
+	// logParentId := dao.ParentAgentLog(logParent)
 
-	StatusCodes := make([]string, 0)
+	// StatusCodes := make([]string, 0)
 
-	for _, uuid := range fb.UUID {
-		agent := agentmanager.GetAgent(uuid)
-		if agent == nil {
-			log := model.AgentLog{
-				LogParentID:     logParentId,
-				IP:              dao.UUID2MacIP(uuid),
-				OperationObject: filename,
-				Action:          model.BroadcastFile,
-				StatusCode:      http.StatusBadRequest,
-				Message:         "获取uuid失败",
-			}
-			dao.AgentLog(log)
+	// for _, uuid := range fb.UUID {
+	// 	agent := agentmanager.GetAgent(uuid)
+	// 	if agent == nil {
+	// 		log := model.AgentLog{
+	// 			LogParentID:     logParentId,
+	// 			IP:              dao.UUID2MacIP(uuid),
+	// 			OperationObject: filename,
+	// 			Action:          model.BroadcastFile,
+	// 			StatusCode:      http.StatusBadRequest,
+	// 			Message:         "获取uuid失败",
+	// 		}
+	// 		dao.AgentLog(log)
 
-			StatusCodes = append(StatusCodes, strconv.Itoa(http.StatusBadRequest))
-			continue
-		}
+	// 		StatusCodes = append(StatusCodes, strconv.Itoa(http.StatusBadRequest))
+	// 		continue
+	// 	}
 
-		result, Err, err := agent.UpdateFile(path, filename, text)
-		if len(Err) != 0 || err != nil {
-			log := model.AgentLog{
-				LogParentID:     logParentId,
-				IP:              dao.UUID2MacIP(uuid),
-				OperationObject: filename,
-				Action:          model.BroadcastFile,
-				StatusCode:      http.StatusBadRequest,
-				Message:         Err,
-			}
-			dao.AgentLog(log)
+	// 	result, Err, err := agent.UpdateFile(path, filename, text)
+	// 	if len(Err) != 0 || err != nil {
+	// 		log := model.AgentLog{
+	// 			LogParentID:     logParentId,
+	// 			IP:              dao.UUID2MacIP(uuid),
+	// 			OperationObject: filename,
+	// 			Action:          model.BroadcastFile,
+	// 			StatusCode:      http.StatusBadRequest,
+	// 			Message:         Err,
+	// 		}
+	// 		dao.AgentLog(log)
 
-			StatusCodes = append(StatusCodes, strconv.Itoa(http.StatusBadRequest))
-			continue
-		} else {
-			log := model.AgentLog{
-				LogParentID:     logParentId,
-				IP:              dao.UUID2MacIP(uuid),
-				OperationObject: filename,
-				Action:          model.BroadcastFile,
-				StatusCode:      http.StatusOK,
-				Message:         "配置文件下发成功",
-			}
-			dao.AgentLog(log)
+	// 		StatusCodes = append(StatusCodes, strconv.Itoa(http.StatusBadRequest))
+	// 		continue
+	// 	} else {
+	// 		log := model.AgentLog{
+	// 			LogParentID:     logParentId,
+	// 			IP:              dao.UUID2MacIP(uuid),
+	// 			OperationObject: filename,
+	// 			Action:          model.BroadcastFile,
+	// 			StatusCode:      http.StatusOK,
+	// 			Message:         "配置文件下发成功",
+	// 		}
+	// 		dao.AgentLog(log)
 
-			// 保存历史版本
-			time := service.NowTime()
-			ip, _, ipdept := dao.MachineBasic(uuid)
-			fd := model.HistoryFiles{
-				IP:       ip,
-				IPDept:   ipdept,
-				UUID:     uuid,
-				Path:     path,
-				FileName: filename + "-" + time,
-				File:     result.(string),
-			}
-			dao.SaveHistoryFile(fd)
+	// 		// 保存历史版本
+	// 		time := service.NowTime()
+	// 		ip, _, ipdept := dao.MachineBasic(uuid)
+	// 		fd := model.HistoryFiles{
+	// 			IP:       ip,
+	// 			IPDept:   ipdept,
+	// 			UUID:     uuid,
+	// 			Path:     path,
+	// 			FileName: filename + "-" + time,
+	// 			File:     result.(string),
+	// 		}
+	// 		dao.SaveHistoryFile(fd)
 
-			StatusCodes = append(StatusCodes, strconv.Itoa(http.StatusOK))
-		}
-	}
-	status := service.BatchActionStatus(StatusCodes)
-	dao.UpdateParentAgentLog(logParentId, status)
-	response.Success(c, nil, "配置文件下发完成!")
+	// 		StatusCodes = append(StatusCodes, strconv.Itoa(http.StatusOK))
+	// 	}
+	// }
+	// status := service.BatchActionStatus(StatusCodes)
+	// dao.UpdateParentAgentLog(logParentId, status)
+	// response.Success(c, nil, "配置文件下发完成!")
 }
---
+++ pkg/app/server/controller/filehandler.go
@@ -0,0 +1,191 @@
+/******************************************************************************
+ * Copyright (c) KylinSoft Co., Ltd.2021-2022. All rights reserved.
+ * PilotGo is licensed under the Mulan PSL v2.
+ * You can use this software accodring to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *     http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN 'AS IS' BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ * Author: zhanghan
+ * Date: 2022-06-16 10:25:52
+ * LastEditTime: 2022-06-16 16:16:10
+ * Description: file info handler
+ ******************************************************************************/
+package controller
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"openeluer.org/PilotGo/PilotGo/pkg/app/server/dao"
+	"openeluer.org/PilotGo/PilotGo/pkg/app/server/model"
+	"openeluer.org/PilotGo/PilotGo/pkg/utils/response"
+)
+
+func SaveFileToDatabase(c *gin.Context) {
+	var file model.Files
+	c.Bind(&file)
+
+	filename := file.FileName
+	if len(filename) == 0 {
+		response.Fail(c, nil, "请输入配置文件名字")
+		return
+	}
+	if !dao.IsExistFile(filename) {
+		response.Fail(c, nil, "文件名字已存在，请重新输入")
+		return
+	}
+
+	filetype := file.Type
+	if len(filetype) == 0 {
+		response.Fail(c, nil, "请选择文件类型")
+		return
+	}
+
+	description := file.Description
+	if len(description) == 0 {
+		response.Fail(c, nil, "请添加文件描述")
+		return
+	}
+
+	batchId := file.ControlledBatch
+
+	text := file.File
+	if len(text) == 0 {
+		response.Fail(c, nil, "请重新检查文件内容")
+		return
+	}
+
+	fd := model.Files{
+		UserUpdate:      file.UserUpdate,
+		UserDept:        file.UserDept,
+		FileName:        filename,
+		Type:            filetype,
+		Description:     description,
+		ControlledBatch: batchId,
+		File:            text,
+	}
+	dao.SaveFile(fd)
+	response.Success(c, nil, "文件保存成功")
+}
+
+func DeleteFile(c *gin.Context) {
+	var files model.DeleteFiles
+	c.Bind(&files)
+
+	for _, fileId := range files.FileIDs {
+		dao.DeleteFile(fileId)
+		dao.DeleteHistoryFile(fileId)
+	}
+	response.Success(c, nil, "储存的文件已从数据库中删除")
+}
+
+func UpdateFile(c *gin.Context) {
+	var file model.Files
+	c.Bind(&file)
+
+	id := file.ID
+	dao.SaveHistoryFile(id)
+
+	user := file.UserUpdate
+	userDept := file.UserDept
+	filename := file.FileName
+	description := file.Description
+
+	batchId := file.ControlledBatch
+	text := file.File
+	if !dao.IsExistId(file.ID) {
+		response.Fail(c, nil, "id有误,请重新确认该文件是否存在")
+		return
+	}
+	f := model.Files{
+		Type:            file.Type,
+		FileName:        filename,
+		Description:     description,
+		UserUpdate:      user,
+		UserDept:        userDept,
+		ControlledBatch: batchId,
+		File:            text,
+	}
+	dao.UpdateFile(id, f)
+
+	response.Success(c, nil, "配置文件修改成功")
+}
+
+func AllFiles(c *gin.Context) {
+	query := &model.PaginationQ{}
+	err := c.ShouldBindQuery(query)
+	if err != nil {
+		response.Response(c, http.StatusOK, http.StatusBadRequest, gin.H{"status": false}, err.Error())
+		return
+	}
+
+	files := model.Files{}
+	list, tx := files.AllFiles(query)
+
+	total, err := CrudAll(query, tx, list)
+	if err != nil {
+		response.Fail(c, gin.H{"status": false}, err.Error())
+		return
+	}
+	var filetype []string
+	filetype = append(filetype, model.ConfigRepo)
+	c.AbortWithStatusJSON(http.StatusOK, gin.H{
+		"code":  http.StatusOK,
+		"ok":    true,
+		"data":  list,
+		"total": total,
+		"page":  query.CurrentPageNum,
+		"size":  query.Size,
+		"type":  filetype})
+}
+
+func FileSearch(c *gin.Context) {
+	var file model.SearchFile
+	c.Bind(&file)
+	search := file.Search
+
+	query := &model.PaginationQ{}
+	err := c.ShouldBindQuery(query)
+	if err != nil {
+		response.Response(c, http.StatusOK, http.StatusBadRequest, gin.H{"status": false}, err.Error())
+		return
+	}
+
+	list, tx := file.FileSearch(query, search)
+
+	total, err := CrudAll(query, tx, list)
+	if err != nil {
+		response.Response(c, http.StatusOK, http.StatusBadRequest, gin.H{"status": false}, err.Error())
+		return
+	}
+	JsonPagination(c, list, total, query)
+}
+
+func HistoryFiles(c *gin.Context) {
+	query := &model.PaginationQ{}
+	err := c.ShouldBindQuery(query)
+	if err != nil {
+		response.Response(c, http.StatusOK, http.StatusBadRequest, gin.H{"status": false}, err.Error())
+		return
+	}
+
+	fileId := c.Query("id")
+	FileId, err := strconv.Atoi(fileId)
+	if err != nil {
+		response.Fail(c, nil, "文件ID输入格式有误")
+		return
+	}
+
+	files := model.HistoryFiles{}
+	list, tx := files.HistoryFiles(query, FileId)
+
+	total, err := CrudAll(query, tx, list)
+	if err != nil {
+		response.Fail(c, gin.H{"status": false}, err.Error())
+		return
+	}
+	JsonPagination(c, list, total, query)
+}
