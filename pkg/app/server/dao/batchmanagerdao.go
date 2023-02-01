42f55b6246015f668619a3cb9030e498bf2c1aa0
wubijie <wubijie@kylinos.cn>
2023-02-01 16:21:16 +0800

    Added database error handling to the batchmanagerdao file

--- pkg/app/server/dao/batchmanagerdao.go
+++ pkg/app/server/dao/batchmanagerdao.go
@@ -5,74 +5,77 @@ import (
 
 	"openeuler.org/PilotGo/PilotGo/pkg/app/server/model"
 	"openeuler.org/PilotGo/PilotGo/pkg/global"
+	"openeuler.org/PilotGo/PilotGo/pkg/logger"
 	"openeuler.org/PilotGo/PilotGo/pkg/utils"
 )
 
 func IsExistName(name string) (bool, error) {
 	var batch model.Batch
 	err := global.PILOTGO_DB.Where("name=?", name).Find(&batch).Error
-	if err != nil {
-		return batch.ID != 0, err
-	}
-	return batch.ID != 0, nil
+	return batch.ID != 0, err
 }
-func IsExistID(id int) bool {
+func IsExistID(id int) (bool, error) {
 	var batch model.Batch
-	global.PILOTGO_DB.Where("id=?", id).Find(&batch)
-	return batch.ID != 0
+	err := global.PILOTGO_DB.Where("id=?", id).Find(&batch).Error
+	return batch.ID != 0, err
 }
-func GetBatchID(name string) uint {
+func GetBatchID(name string) (uint, error) {
 	var batch model.Batch
-	global.PILOTGO_DB.Where("name=?", name).Find(&batch)
-	return batch.ID
+	err := global.PILOTGO_DB.Where("name=?", name).Find(&batch).Error
+	return batch.ID, err
 }
 
-func DeleteBatch(departid int) {
+func DeleteBatch(departid int) error {
 	var batch model.Batch
-	global.PILOTGO_DB.Where("id=?", departid).Unscoped().Delete(&batch)
+	return global.PILOTGO_DB.Where("id=?", departid).Unscoped().Delete(&batch).Error
 }
 
-func UpdateBatch(BatchID int, BatchName string, Descrip string) {
+func UpdateBatch(BatchID int, BatchName string, Descrip string) error {
 	var Batch model.Batch
 	BatchNew := model.Batch{
 		Name:        BatchName,
 		Description: Descrip,
 	}
-	global.PILOTGO_DB.Model(&Batch).Where("id=?", BatchID).Updates(&BatchNew)
+	return global.PILOTGO_DB.Model(&Batch).Where("id=?", BatchID).Updates(&BatchNew).Error
 }
 
-func GetMachineID(BatchID int) []string {
+func GetMachineID(BatchID int) ([]string, error) {
 	var Batch model.Batch
-	global.PILOTGO_DB.Where("id=?", BatchID).Find(&Batch)
+	err := global.PILOTGO_DB.Where("id=?", BatchID).Find(&Batch).Error
 	str := strings.Split(Batch.Machinelist, ",")
-	return str
+	return str, err
 }
 
 // 创建批次
-func CreateBatch(batch model.Batch) {
-	global.PILOTGO_DB.Create(&batch)
+func CreateBatch(batch model.Batch) error {
+	return global.PILOTGO_DB.Create(&batch).Error
 }
 
 // 根据批次id获取所属的所有uuids
 func BatchIds2UUIDs(batchIds []int) (uuids []string) {
 	for _, batchId := range batchIds {
 		var batch model.Batch
-		global.PILOTGO_DB.Where("id=?", batchId).Find(&batch)
-
+		err := global.PILOTGO_DB.Where("id=?", batchId).Find(&batch).Error
+		if err != nil {
+			logger.Error(err.Error())
+		}
 		str := strings.Split(batch.Machinelist, ",")
 		macIds := utils.String2Int(str)
 
 		for _, macId := range macIds {
 			var machine model.MachineNode
-			global.PILOTGO_DB.Where("id=?", macId).Find(&machine)
+			err = global.PILOTGO_DB.Where("id=?", macId).Find(&machine).Error
+			if err != nil {
+				logger.Error(err.Error())
+			}
 			uuids = append(uuids, machine.MachineUUID)
 		}
 	}
 	return
 }
 
-func GetBatch() []model.Batch {
+func GetBatch() ([]model.Batch, error) {
 	var batch []model.Batch
-	global.PILOTGO_DB.Find(&batch)
-	return batch
+	err := global.PILOTGO_DB.Find(&batch).Error
+	return batch, err
 }
