15262f190d4d466028a42ab56108d9f556a56306
Wangjunqi123 <wangjunqi@kylinos.cn>
2023-04-24 16:08:10 +0800

    change runcommand() in os/baseos/sysctl.go

--- pkg/utils/os/baseos/sysctl.go
+++ pkg/utils/os/baseos/sysctl.go
@@ -9,47 +9,48 @@ import (
 )
 
 func (b *BaseOS) GetSysctlConfig() (map[string]string, error) {
-	tmp, err := utils.RunCommand("sysctl -a")
-	if err != nil {
-		logger.Error("Failed to retrieve the kernel configuration file: %s", err.Error())
-		return nil, err
-	}
+	exitc, tmp, stde, err := utils.RunCommandnew("sysctl -a")
+	if exitc == 0 && tmp != "" && stde == "" && err == nil {
+		sysConfig := make(map[string]string)
+		lines := strings.Split(tmp, "\n")
+		for _, line := range lines {
+			line = strings.TrimSpace(line)
+			if line == "" {
+				continue
+			}
 
-	sysConfig := make(map[string]string)
-	lines := strings.Split(tmp, "\n")
-	for _, line := range lines {
-		line = strings.TrimSpace(line)
-		if line == "" {
-			continue
+			strSlice := strings.Split(line, " =")
+			key := strSlice[0]
+			value := strings.TrimLeft(line[len(key)+2:], " ")
+			sysConfig[key] = value
 		}
-
-		strSlice := strings.Split(line, " =")
-		key := strSlice[0]
-		value := strings.TrimLeft(line[len(key)+2:], " ")
-		sysConfig[key] = value
+		return sysConfig, nil
 	}
-	return sysConfig, nil
+	logger.Error("failed to retrieve the kernel configuration file: %d, %s, %s, %v", exitc, tmp, stde, err)
+	return nil, fmt.Errorf("failed to retrieve the kernel configuration file: %d, %s, %s, %v", exitc, tmp, stde, err)
+
 }
 
 // sysctl -w net.ipv4.ip_forward=1  临时修改系统参数
 func (b *BaseOS) TempModifyPar(arg string) (string, error) {
-	tmp, err := utils.RunCommand(fmt.Sprintf("sudo sysctl -w %s", arg))
-	tmp = strings.Replace(tmp, "\n", "", -1)
-
-	if err != nil {
-		logger.Error("failed to modify the kernel runtime parameters: %s", err.Error())
-		return tmp, fmt.Errorf("failed to modify the kernel runtime parameters: %s", err.Error())
+	exitc, tmp, stde, err := utils.RunCommandnew(fmt.Sprintf("sudo sysctl -w %s", arg))
+	if exitc == 0 && tmp != "" && stde == "" && err == nil {
+		tmp = strings.Replace(tmp, "\n", "", -1)
+		return tmp, nil
 	}
-	return tmp, nil
+	logger.Error("failed to modify the kernel runtime parameters: %d, %s, %s, %v", exitc, tmp, stde, err)
+	return "", fmt.Errorf("failed to modify the kernel runtime parameters: %d, %s, %s, %v", exitc, tmp, stde, err)
+
 }
 
 // sysctl -n net.ipv4.ip_forward  查看某个内核参数的值
 func (b *BaseOS) GetVarNameValue(arg string) (string, error) {
-	tmp, err := utils.RunCommand(fmt.Sprintf("sysctl -n %s", arg))
-	tmp = strings.Replace(tmp, "\n", "", -1)
-	if err != nil {
-		logger.Error("failed to get the value of the parameter: %s", err.Error())
-		return tmp, fmt.Errorf("failed to get the value of the parameter: %s", err.Error())
+	exitc, tmp, stde, err := utils.RunCommandnew(fmt.Sprintf("sysctl -n %s", arg))
+	if exitc == 0 && tmp != "" && stde == "" && err == nil {
+		tmp = strings.Replace(tmp, "\n", "", -1)
+		return tmp, nil
 	}
-	return tmp, nil
+	logger.Error("failed to get the value of the parameter: %d, %s, %s, %v", exitc, tmp, stde, err)
+	return "", fmt.Errorf("failed to get the value of the parameter: %d, %s, %s, %v", exitc, tmp, stde, err)
+
 }
