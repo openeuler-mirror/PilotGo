665845706f735bf218c543f0c41e7c395eaa7efb
wubijie <wubijie@kylinos.cn>
2023-04-26 14:39:00 +0800

    add message data bind to struct of FirewalldZoneConfig

--- pkg/utils/os/baseos/firewall_test.go
+++ pkg/utils/os/baseos/firewall_test.go
@@ -25,7 +25,6 @@ func TestFirewall(t *testing.T) {
 		osobj.FirewalldSetDefaultZone(defaultzone)
 		tmp, err := osobj.Config()
 		assert.Nil(t, err)
-		//new_config := tmp.(map[string]interface{})
 		new_zone := tmp.DefaultZone
 		assert.Equal(t, defaultzone, new_zone)
 		osobj.FirewalldSetDefaultZone("public")
@@ -35,7 +34,6 @@ func TestFirewall(t *testing.T) {
 		osobj.Stop()
 		tmp, err := osobj.Config()
 		assert.Nil(t, err)
-		//new_config := tmp.(map[string]interface{})
 		new_status := tmp.Status
 		assert.Equal(t, "not running", new_status)
 	})
@@ -44,61 +42,54 @@ func TestFirewall(t *testing.T) {
 		osobj.Restart()
 		tmp, err := osobj.Config()
 		assert.Nil(t, err)
-		//new_config := tmp.(map[string]interface{})
 		new_status := tmp.Status
 		assert.Equal(t, "running", new_status)
 	})
 
 	t.Run("test FirewalldSourceAdd", func(t *testing.T) {
 		osobj.FirewalldSourceAdd(dropzone, "192.168.75.200")
-		tmp, err := osobj.FirewalldZoneConfig(dropzone)
+		new_zoneconfig, err := osobj.FirewalldZoneConfig(dropzone)
 		assert.Nil(t, err)
-		new_zoneconfig := tmp.(*FirewalldCMDList)
 		new_sources := new_zoneconfig.Sources
 		assert.Equal(t, []string{"192.168.75.200"}, new_sources)
 	})
 
 	t.Run("test FirewalldSourceRemove", func(t *testing.T) {
 		osobj.FirewalldSourceRemove(dropzone, "192.168.75.200")
-		tmp, err := osobj.FirewalldZoneConfig(dropzone)
+		new_zoneconfig, err := osobj.FirewalldZoneConfig(dropzone)
 		assert.Nil(t, err)
-		new_zoneconfig := tmp.(*FirewalldCMDList)
 		new_sources := new_zoneconfig.Sources
 		assert.Equal(t, []string{""}, new_sources)
 	})
 
 	t.Run("test FirewalldServiceAdd", func(t *testing.T) {
 		osobj.FirewalldServiceAdd(dropzone, "dns")
-		tmp, err := osobj.FirewalldZoneConfig(dropzone)
+		new_zoneconfig, err := osobj.FirewalldZoneConfig(dropzone)
 		assert.Nil(t, err)
-		new_zoneconfig := tmp.(*FirewalldCMDList)
 		new_service := new_zoneconfig.Service
 		assert.Equal(t, []string{"dns"}, new_service)
 	})
 
 	t.Run("test FirewalldServiceRemove", func(t *testing.T) {
 		osobj.FirewalldServiceRemove(dropzone, "dns")
-		tmp, err := osobj.FirewalldZoneConfig(dropzone)
+		new_zoneconfig, err := osobj.FirewalldZoneConfig(dropzone)
 		assert.Nil(t, err)
-		new_zoneconfig := tmp.(*FirewalldCMDList)
 		new_service := new_zoneconfig.Service
 		assert.Equal(t, []string{""}, new_service)
 	})
 
 	t.Run("test AddZonePort", func(t *testing.T) {
 		osobj.AddZonePort(dropzone, "53", "tcp")
-		tmp, err := osobj.FirewalldZoneConfig(dropzone)
+		new_zoneconfig, err := osobj.FirewalldZoneConfig(dropzone)
 		assert.Nil(t, err)
-		new_zoneconfig := tmp.(*FirewalldCMDList)
 		new_ports := new_zoneconfig.Ports.([]map[string]string)
 		assert.Equal(t, []map[string]string{{"port": "53", "protocol": "tcp"}}, new_ports)
 	})
 
 	t.Run("test DelZonePort", func(t *testing.T) {
 		osobj.DelZonePort(dropzone, "53", "tcp")
-		tmp, err := osobj.FirewalldZoneConfig(dropzone)
+		new_zoneconfig, err := osobj.FirewalldZoneConfig(dropzone)
 		assert.Nil(t, err)
-		new_zoneconfig := tmp.(*FirewalldCMDList)
 		new_ports := new_zoneconfig.Ports.([]map[string]string)
 		assert.Equal(t, []map[string]string{}, new_ports)
 	})
