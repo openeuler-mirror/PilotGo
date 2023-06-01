d076f00ccebfdc287ed7dea540833fc903215a7a
wubijie <wubijie@kylinos.cn>
2023-06-01 15:32:02 +0800

    Add audit logs for the adduser methods

--- pkg/app/server/controller/userhandler.go
+++ pkg/app/server/controller/userhandler.go
@@ -21,6 +21,7 @@ import (
 	"github.com/tealeg/xlsx"
 	"openeuler.org/PilotGo/PilotGo/pkg/app/server/dao"
 	"openeuler.org/PilotGo/PilotGo/pkg/app/server/service"
+	"openeuler.org/PilotGo/PilotGo/pkg/app/server/service/auditlog"
 	"openeuler.org/PilotGo/PilotGo/pkg/utils/response"
 )
 
@@ -34,16 +35,25 @@ func GetUserRoleHandler(c *gin.Context) {
 }
 
 func RegisterHandler(c *gin.Context) {
-	var user dao.User
+	var user service.User
 	if c.Bind(&user) != nil {
 		response.Fail(c, nil, "parameter error")
 		return
 	}
+
+	//TODO:
+	var user1 service.User
+	log := auditlog.NewAuditLog(auditlog.LogTypeUser, "添加用户", "", user1)
+	auditlog.AddAuditLog(log)
+
 	err := service.Register(user)
 	if err != nil {
+		auditlog.UpdateStatus(log, auditlog.StatusFail)
 		response.Fail(c, nil, err.Error())
 		return
 	}
+
+	auditlog.UpdateStatus(log, auditlog.StatusSuccess)
 	response.Success(c, nil, "添加用户成功!") //Return result
 }
 
