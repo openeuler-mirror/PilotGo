5a7bf1aba5566eab4b682d45a00fe9694c9ae852
Gzx1999 <guozhengxin@kylinos.cn>
2023-08-31 09:15:52 +0800

    add agent message defines; remove network config

--- frontend/vue/views/Cluster/detail/network.vue
+++ frontend/vue/views/Cluster/detail/network.vue
@@ -9,33 +9,29 @@
   See the Mulan PSL v2 for more details.
   Author: zhaozhenfang
   Date: 2022-04-11 13:07:46
- LastEditTime: 2023-04-18 09:46:14
+ LastEditTime: 2023-08-30 17:31:24
  -->
 <template>
   <div class="content">
-    <div class="operation">
-     <el-button plain  type="primary" size="medium" @click="handleEdit">编辑</el-button>
-   </div><br/>
-   <h3>IPv4:</h3><br/>
-   <el-descriptions :column="2" size="medium" border>
-    <el-descriptions-item label="IP分配">{{ net.BOOTPROTO === 'dhcp'? '动态DHCP' : '手动' }}</el-descriptions-item>
-    <el-descriptions-item label="IPv4地址">{{ net.IPADDR }}</el-descriptions-item>
-    <el-descriptions-item label="IPv4子网掩码">{{ net.NETMASK || '无'}}</el-descriptions-item>
-    <el-descriptions-item label="IPv4网关">{{ net.GATEWAY }}</el-descriptions-item>
-    <el-descriptions-item label="IPv4首选DNS">{{net.DNS1}}</el-descriptions-item>
-    <el-descriptions-item label="IPv4备选DNS">{{net.DNS2 || '无'}}</el-descriptions-item>
+    <!-- <div class="operation">
+      <el-button plain type="primary" size="medium" @click="handleEdit">编辑</el-button>
+    </div> -->
+    <br />
+    <h3>IPv4:</h3>
+    <br />
+    <el-descriptions :column="2" size="medium" border>
+      <el-descriptions-item label="IP分配">{{ net.BOOTPROTO === 'dhcp' ? '动态DHCP' : '手动' }}</el-descriptions-item>
+      <el-descriptions-item label="IPv4地址">{{ net.IPADDR }}</el-descriptions-item>
+      <el-descriptions-item label="IPv4子网掩码">{{ net.NETMASK || '无' }}</el-descriptions-item>
+      <el-descriptions-item label="IPv4网关">{{ net.GATEWAY }}</el-descriptions-item>
+      <el-descriptions-item label="IPv4首选DNS">{{ net.DNS1 }}</el-descriptions-item>
+      <el-descriptions-item label="IPv4备选DNS">{{ net.DNS2 || '无' }}</el-descriptions-item>
     </el-descriptions>
 
-   <el-dialog 
-    :title="title"
-    top="10vh"
-    :before-close="handleClose" 
-    :visible.sync="display" 
-    width="70%"
-  >  
-    <update-form v-if="type === 'update'" :net="net" @click="handleClose"></update-form>      
-  </el-dialog>
- </div>
+    <el-dialog :title="title" top="10vh" :before-close="handleClose" :visible.sync="display" width="70%">
+      <update-form v-if="type === 'update'" :net="net" @click="handleClose"></update-form>
+    </el-dialog>
+  </div>
 </template>
 <script>
 import { getNetwork } from '@/request/cluster';
@@ -63,14 +59,14 @@ export default {
     }
   },
   mounted() {
-    if(this.$route.params.detail != undefined) {
-    getNetwork({uuid:this.$route.params.detail}).then(res => {
-      if(res.data.code === 200) {
-        this.net = res.data.data;
-      } else {
-        console.log(res.data.msg)
-      }
-    })
+    if (this.$route.params.detail != undefined) {
+      getNetwork({ uuid: this.$route.params.detail }).then(res => {
+        if (res.data.code === 200) {
+          this.net = res.data.data;
+        } else {
+          console.log(res.data.msg)
+        }
+      })
     }
   },
   methods: {
@@ -90,13 +86,15 @@ export default {
 </script>
 <style scoped lang="scss">
 .content {
-  width:96%; 
-  padding-top:20px; 
+  width: 96%;
+  padding-top: 20px;
   margin: 0 auto;
+
   .operation {
     width: 100%;
     text-align: right;
   }
+
   h3 {
     color: rgb(145, 139, 139);
   }
