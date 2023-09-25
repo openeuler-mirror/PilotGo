bd8d43be5ac5a313577afff4d38531c3a6130e87
zhanghan2021 <zhanghan@kylinos.cn>
2023-09-25 16:34:20 +0800

    update file service upload func for bin data flow

--- pkg/app/server/controller/fileservice.go
+++ pkg/app/server/controller/fileservice.go
@@ -3,48 +3,87 @@ package controller
 import (
 	"fmt"
 	"io"
+	"io/ioutil"
+	"net/url"
 	"os"
 	"path/filepath"
 
 	"github.com/gin-gonic/gin"
+	"openeuler.org/PilotGo/PilotGo/pkg/logger"
 	"openeuler.org/PilotGo/PilotGo/pkg/utils/response"
 )
 
 func Upload(c *gin.Context) {
-	file, header, err := c.Request.FormFile("file")
-	if err != nil {
-		response.Fail(c, gin.H{"error": err.Error()}, "上传失败")
-		return
+	contentType := c.Request.Header.Get("Content-Type")
+
+	if contentType == "multipart/form-data" {
+		// 直接读取request body内容
+		bodyBuf, err := ioutil.ReadAll(c.Request.Body)
+		if err != nil {
+			logger.Error("没获取到request body: %s", err.Error())
+			response.Fail(c, gin.H{"error": err.Error()}, "获取文件request body失败")
+			return
+		}
+
+		parsedURL, err := url.Parse(c.Request.RequestURI)
+		if err != nil {
+			logger.Error("解析 URL 错误:%v", err.Error())
+			response.Fail(c, gin.H{"error": err.Error()}, "解析URI失败")
+			return
+		}
+		filename := parsedURL.Query().Get("filename")
+
+		uploadPath := c.DefaultQuery("path", "./uploads")            // 获取上传文件的保存路径，默认是 ./uploads，可以通过path设置上传路径
+		if err := os.MkdirAll(uploadPath, os.ModePerm); err != nil { // 确保保存路径存在，如果不存在则创建
+			response.Fail(c, gin.H{"error": err.Error()}, "创建保存路径失败")
+			return
+		}
+		destination := filepath.Join(uploadPath, filename) // 上传文件的目标路径
+
+		outFile, err := os.Create(destination) // 创建并打开目标文件
+		if err != nil {
+			response.Fail(c, gin.H{"error": err.Error()}, "创建目标文件失败")
+			return
+		}
+		defer outFile.Close()
+
+		_, err = outFile.Write(bodyBuf) // 将请求体中的二进制数据写入目标文件
+		if err != nil {
+			response.Fail(c, gin.H{"error": err.Error()}, "文件保存失败")
+			return
+		}
+
+	} else {
+		// api测试工具及网页前端上传文件
+		file, header, err := c.Request.FormFile("file")
+		if err != nil {
+			response.Fail(c, gin.H{"error": err.Error()}, "未获取到文件")
+			return
+		}
+		defer file.Close()
+
+		uploadPath := c.DefaultQuery("path", "./uploads") // 获取上传文件的保存路径，默认是 ./uploads，可以通过path设置上传路径
+
+		if err := os.MkdirAll(uploadPath, os.ModePerm); err != nil { // 确保保存路径存在，如果不存在则创建
+			response.Fail(c, gin.H{"error": err.Error()}, "保存路径创建失败")
+			return
+		}
+
+		destination := filepath.Join(uploadPath, header.Filename) // 上传文件的目标路径
+
+		outFile, err := os.Create(destination) // 创建目标文件
+		if err != nil {
+			response.Fail(c, gin.H{"error": err.Error()}, "目标文件创建失败")
+			return
+		}
+		defer outFile.Close()
+
+		_, err = io.Copy(outFile, file) // 复制上传的文件到目标文件
+		if err != nil {
+			response.Fail(c, gin.H{"error": err.Error()}, "文件保存失败")
+			return
+		}
 	}
-	defer file.Close()
-
-	// 获取上传文件的保存路径，默认是 ./uploads，可以通过path设置上传路径
-	uploadPath := c.DefaultQuery("path", "./uploads")
-
-	// 确保保存路径存在，如果不存在则创建
-	if err := os.MkdirAll(uploadPath, os.ModePerm); err != nil {
-		response.Fail(c, gin.H{"error": err.Error()}, "上传失败")
-		return
-	}
-
-	// 指定上传文件的目标路径
-	destination := filepath.Join(uploadPath, header.Filename)
-
-	// 创建目标文件
-	outFile, err := os.Create(destination)
-	if err != nil {
-		response.Fail(c, gin.H{"error": err.Error()}, "上传失败")
-		return
-	}
-	defer outFile.Close()
-
-	// 复制上传的文件到目标文件
-	_, err = io.Copy(outFile, file)
-	if err != nil {
-		response.Fail(c, gin.H{"error": err.Error()}, "上传失败")
-		return
-	}
-
 	response.Success(c, nil, "文件上传成功")
 }
 
