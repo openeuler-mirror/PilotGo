c699d4b7b7ab4b65d315556566191bc5f1a4737b
Gzx1999 <guozhengxin@kylinos.cn>
2023-10-10 17:49:46 +0800

    add event service framework

--- sdk/plugin/client/event.go
+++ sdk/plugin/client/event.go
@@ -4,18 +4,14 @@ import (
 	"encoding/json"
 	"errors"
 
+	"gitee.com/openeuler/PilotGo/sdk/common"
 	"gitee.com/openeuler/PilotGo/sdk/utils/httputils"
 )
 
-type Event struct {
-	ID       int
-	MetaData interface{}
-}
-
-type EventCallback func(e *Event)
+type EventCallback func(e *common.EventMessage)
 
 // 注册event事件监听
-func (c *Client) ListenEvent(event Event, callback EventCallback) error {
+func (c *Client) ListenEvent(eventType int, callback EventCallback) error {
 	url := c.Server + "/api/v1/pluginapi/listener"
 	r, err := httputils.Put(url, nil)
 	if err != nil {
@@ -33,7 +29,8 @@ func (c *Client) ListenEvent(event Event, callback EventCallback) error {
 		return errors.New(resp.Error)
 	}
 
-	// TODO: register event handler here
+	c.registerEventCallback(eventType, callback)
+
 	return nil
 }
 
@@ -58,4 +55,27 @@ func (c *Client) UnListenEvent(listenerID string) error {
 
 	// TODO: unregister event handler here
 	return nil
+}
+
+// 发布event事件
+func (c *Client) PublishEvent(msg common.EventMessage) error {
+	// TODO:
+	return errors.New("not implemented")
+}
+
+func (c *Client) registerEventCallback(eventType int, callback EventCallback) {
+	// TODO:
+}
+
+func (c *Client) ProcessEvent(event *common.EventMessage) {
+	c.eventChan <- event
+}
+
+func (c *Client) startEventProcessor() {
+	for {
+		e := <-c.eventChan
+
+		// TODO: process event message
+		e = e
+	}
 }
--- src/app/server/service/eventbus/eventbus.go
+++ src/app/server/service/eventbus/eventbus.go
@@ -3,6 +3,7 @@ package eventbus
 import (
 	"sync"
 
+	"gitee.com/openeuler/PilotGo/sdk/common"
 	"gitee.com/openeuler/PilotGo/sdk/logger"
 )
 
@@ -11,38 +12,12 @@ type Listener struct {
 	URL  string
 }
 
-const (
-	// 主机安装软件包
-	MsgPackageInstall = 0
-	// 主机升级软件包
-	MsgPackageUpdate = 1
-	// 主机卸载软件包
-	MsgPackageUninstall = 2
-	// 主机ip变更
-	MsgIPChange = 3
-
-	// 平台新增主机
-	MsgHostAdd = 10
-	// 平台移除主机
-	MsgHostRemove = 11
-
-	// 插件添加
-	MsgPluginAdd = 20
-	// 插件卸载
-	MsgPluginRemove = 21
-)
-
-type EventMessage struct {
-	MessageType int
-	MessageData interface{}
-}
-
 type EventBus struct {
 	sync.Mutex
 	listeners []*Listener
 	stop      chan struct{}
 	wait      sync.WaitGroup
-	event     chan *EventMessage
+	event     chan *common.EventMessage
 }
 
 func (e *EventBus) AddListener(l *Listener) {
@@ -84,7 +59,7 @@ func (e *EventBus) Stop() {
 	e.wait.Wait()
 }
 
-func (e *EventBus) publish(m *EventMessage) {
+func (e *EventBus) publish(m *common.EventMessage) {
 	e.event <- m
 }
 
@@ -111,6 +86,6 @@ func RemoveListener(l *Listener) {
 	globalEventBus.RemoveListener(l)
 }
 
-func PublishEvent(m *EventMessage) {
+func PublishEvent(m *common.EventMessage) {
 	globalEventBus.publish(m)
 }
