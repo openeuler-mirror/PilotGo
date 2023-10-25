11441d11e47699aff7abf0efe60677bb0a77f546
Gzx1999 <guozhengxin@kylinos.cn>
2023-10-25 15:04:07 +0800

    modify admin user account

--- frontend/vue/rules/check.js
+++ frontend/vue/rules/check.js
@@ -12,14 +12,17 @@
  * @LastEditTime: 2022-04-22 15:16:50
  */
 // 校验ip
-  export let checkIP = (rule, value, callback, obj) => {
-    let reg = /^([1-9]|[1-9]\d|1\d{2}|2[0-1]\d|22[0-3])(\.(\d|[1-9]\d|1\d{2}|2[0-4]\d|25[0-5])){3}$/;
-    regex(rule, value, callback, obj, reg);
-  };
+export let checkIP = (rule, value, callback, obj) => {
+  let reg = /^([1-9]|[1-9]\d|1\d{2}|2[0-1]\d|22[0-3])(\.(\d|[1-9]\d|1\d{2}|2[0-4]\d|25[0-5])){3}$/;
+  regex(rule, value, callback, obj, reg);
+};
 
 // 校验邮箱 /^[1]([3-9])[0-9]{9}$/
 export let checkEmail = (rule, value, callback) => {
   if (!value) {
+    return callback();
+  }
+  if (value === "admin") {
     return callback();
   }
   let patt = /^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*\.[a-zA-Z0-9]{2,6}$/;
@@ -32,15 +35,15 @@ export let checkPhone = (rule, value, callback, obj) => {
   regex(rule, value, callback, obj, reg);
 };
 
-  function regex(rule, value, callback, obj, reg) {
-    if (!value) {
-      callback();
-    }
-    if (!reg.test(value)) {
-      return callback(new Error());
-    }
+function regex(rule, value, callback, obj, reg) {
+  if (!value) {
     callback();
   }
+  if (!reg.test(value)) {
+    return callback(new Error());
+  }
+  callback();
+}
 
 // 校验端口
 export let checkPort = (rule, value, callback) => {
