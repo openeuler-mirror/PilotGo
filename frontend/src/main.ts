5e6bb9b2d979251b3fc03943e49d8737d734cd01
zhaozhenfang <zhaozhenfang@kylinos.cn>
2024-01-24 14:54:27 +0800

    add collapse menu function

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,17 +1,19 @@
-import './assets/main.css'
+import './styles/main.css'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import pinia from '@/stores'
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 import App from './App.vue'
 import router from './router'
+import microApp from '@micro-zoe/micro-app'
+
+
 
 export const app = createApp(App)
-
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
 app.use(ElementPlus)
 
@@ -20,3 +22,4 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
 }
 
 app.mount('#app')
+microApp.start()
--- frontend/src/views/Home/components/Sidebar.vue
+++ frontend/src/views/Home/components/Sidebar.vue
@@ -1,57 +1,69 @@
 <template>
-  <div>
-    <template v-for="item in routes">
-      <router-link :to="item.path">
-        <el-menu-item :index="item.panel" v-if="!item.subMenus">
-          <component class="sidebar_icon" :is="item.icon"></component>
+  <div class="menu">
+    <el-menu :collapse="props.isCollapse" :router="true">
+      <template v-for="(menu, index) in routes" :key="index">
+        <!-- 带子菜单的项 -->
+        <el-sub-menu v-if="menu.subMenus" :index="menu.path">
           <template #title>
-            <span style="padding-left: 2px;">{{ item.title }}</span>
+            <el-icon>
+              <component class="sidebar_icon" :is="menu.icon"></component>
+            </el-icon>
+            <span>{{ menu.title }}</span>
           </template>
-        </el-menu-item>
-      </router-link>
-      <el-sub-menu :index="item.panel" :key="item.title" v-if="item.subMenus">
-        <template #title>
-          <component class="sidebar_icon" :is="item.icon"></component>
-          <span style="padding-left: 2px;">{{ item.title }}</span>
-        </template>
-        <router-link v-for="subItem in item.subMenus" :key="subItem.title" :to="subItem.path">
-          <el-menu-item :index="subItem.title">
-            <span>{{ subItem.title }}</span>
+          <el-menu-item v-for="(subMenu, subIndex) in menu.subMenus" :index="subMenu.path">
+            {{ subMenu.title }}
           </el-menu-item>
-        </router-link>
-      </el-sub-menu>
-    </template>
+        </el-sub-menu>
+        <!-- 不带子菜单的项 -->
+        <el-menu-item v-if="!menu.subMenus" :index="menu.path">
+          <component class="sidebar_icon" :is="menu.icon"></component>
+          <template #title>{{ menu.title }}</template>
+        </el-menu-item>
+      </template>
+    </el-menu>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref, watchEffect, onMounted } from "vue";
 import { routerStore, type Menu } from "@/stores/router";
-
-let routes = ref<Menu[]>([]);
-
+let routes: any = ref<Menu[]>([]);
+let props = defineProps({
+  isCollapse: {
+    type: Boolean,
+    default: false,
+    required: true
+  }
+})
 onMounted(() => {
   routes.value = routerStore().menus
 });
-
+const handleOpen = (_key: string, _keyPath: string[]) => {
+  // console.log(key, keyPath)
+}
+const handleClose = (_key: string, _keyPath: string[]) => {
+  // console.log(key, keyPath)
+}
 watchEffect(() => {
-  routes.value = routerStore().menus
+  routes.value = routerStore().menus;
+  console.log(routes.value)
 })
 </script>
 
 <style lang="scss" scoped>
+.menu {
+  width: 100%;
+  height: 100%;
+}
+
 .sidebar_icon {
   width: 20px;
   height: 20px;
 }
 
 .el-menu-item.is-active {
-  background: #f2f8ff !important;
-  border-right: 3px solid rgb(241, 139, 14);
-  color: rgb(241, 139, 14);
-}
-
-a {
-  text-decoration: none;
+  background: rgb(236, 245, 255);
+  border-right: 2px solid var(--active-color);
+  color: var(--active-color);
 }
 </style>
