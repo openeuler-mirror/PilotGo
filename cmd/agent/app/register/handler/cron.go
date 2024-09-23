1f0cfa6444cffdb0a4faa6d3bef91c3d43875843
zhaojianbin <zhaojianbin@kylinos.cn>
2024-09-23 17:17:05 +0800

    优化CronStartHandler部分逻辑

--- cmd/agent/app/register/handler/cron.go
+++ cmd/agent/app/register/handler/cron.go
@@ -13,30 +13,38 @@ import (
 func CronStartHandler(c *network.SocketClient, msg *protocol.Message) error {
 	logger.Debug("process agent info command:%s", msg.String())
 
-	msgg := msg.Data.(string)
-	message := strings.Split(msgg, ",")
-	id, _ := strconv.Atoi(message[0])
-	spec := message[1]
-	command := message[2]
-
-	err := common.CronStart(id, spec, command)
+	dataStr := msg.Data.(string)
+	parts := strings.Split(dataStr, ",")
+	id, err := strconv.Atoi(parts[0])
 	if err != nil {
-		resp_msg := &protocol.Message{
+		return c.Send(&protocol.Message{
 			UUID:   msg.UUID,
 			Type:   msg.Type,
 			Status: -1,
-			Error:  err.Error(),
-		}
-		return c.Send(resp_msg)
-	} else {
-		resp_msg := &protocol.Message{
-			UUID:   msg.UUID,
-			Type:   msg.Type,
-			Status: 0,
-			Data:   "任务已开始",
-		}
-		return c.Send(resp_msg)
+			Error:  "Invalid ID format",
+		})
 	}
+	spec := parts[1]
+	command := parts[2]
+
+	err = common.CronStart(id, spec, command)
+	status := 0
+	if err != nil {
+		status = -1
+	}
+
+	respMsg := &protocol.Message{
+		UUID:   msg.UUID,
+		Type:   msg.Type,
+		Status: status,
+	}
+	if err != nil {
+		respMsg.Error = err.Error()
+	} else {
+		respMsg.Data = "任务已开始"
+	}
+
+	return c.Send(respMsg)
 }
 
 func CronStopAndDelHandler(c *network.SocketClient, msg *protocol.Message) error {
@@ -48,20 +56,20 @@ func CronStopAndDelHandler(c *network.SocketClient, msg *protocol.Message) error
 
 	err := common.StopAndDel(id)
 	if err != nil {
-		resp_msg := &protocol.Message{
+		respMsg := &protocol.Message{
 			UUID:   msg.UUID,
 			Type:   msg.Type,
 			Status: -1,
 			Error:  err.Error(),
 		}
-		return c.Send(resp_msg)
+		return c.Send(respMsg)
 	} else {
-		resp_msg := &protocol.Message{
+		respMsg := &protocol.Message{
 			UUID:   msg.UUID,
 			Type:   msg.Type,
 			Status: 0,
 			Data:   "任务已暂停",
 		}
-		return c.Send(resp_msg)
+		return c.Send(respMsg)
 	}
 }
