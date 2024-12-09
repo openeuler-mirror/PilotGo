8010e72088fd446d8694511a44a5e4f840edbd03
zhanghan <zhanghan@kylinos.cn>
2024-12-09 09:38:58 +0800

    create etcd client

---
+++ sdk/etcd/client.go
@@ -0,0 +1,54 @@
+package etcd
+
+import (
+	"context"
+	"time"
+
+	clientv3 "go.etcd.io/etcd/client/v3"
+)
+
+type Client struct {
+	client *clientv3.Client
+	ctx    context.Context
+}
+
+// NewClient creates a new etcd client
+func NewClient(endpoints []string, dialTimeout time.Duration) (*Client, error) {
+	cli, err := clientv3.New(clientv3.Config{
+		Endpoints:   endpoints,
+		DialTimeout: dialTimeout,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &Client{
+		client: cli,
+		ctx:    context.Background(),
+	}, nil
+}
+
+// Close closes the client
+func (c *Client) Close() error {
+	return c.client.Close()
+}
+
+// Get gets the value for a key
+func (c *Client) Get(key string) (*clientv3.GetResponse, error) {
+	return c.client.Get(c.ctx, key)
+}
+
+// Put puts a key-value pair
+func (c *Client) Put(key, value string) (*clientv3.PutResponse, error) {
+	return c.client.Put(c.ctx, key, value)
+}
+
+// Delete deletes a key
+func (c *Client) Delete(key string) (*clientv3.DeleteResponse, error) {
+	return c.client.Delete(c.ctx, key)
+}
+
+// Watch watches for changes on a key
+func (c *Client) Watch(key string) clientv3.WatchChan {
+	return c.client.Watch(c.ctx, key)
+}
