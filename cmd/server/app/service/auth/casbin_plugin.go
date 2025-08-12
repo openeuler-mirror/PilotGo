f9fc8d707ddc81916b6b9f8aaadce35e0773ad0e
zhanghan <zhanghan@kylinos.cn>
2025-08-12 16:53:02 +0800

    Adjust the method of changing permission functions

--- cmd/server/app/service/auth/casbin_plugin.go
+++ cmd/server/app/service/auth/casbin_plugin.go
@@ -8,20 +8,23 @@
 package auth
 
 import (
+	"gitee.com/openeuler/PilotGo/pkg/global"
 	"gitee.com/openeuler/PilotGo/sdk/common"
 	"gitee.com/openeuler/PilotGo/sdk/logger"
+	"github.com/mitchellh/mapstructure"
 )
 
 // 添加插件服务权限到列表中
 func AddPluginServicePermission(role string, permissions []common.Permission, serviceName string) error {
 	//TODO；先添加到列表中可以展示
 	for _, v := range permissions {
+		PermissionListMap.Set(serviceName, v.Operate, v.Resource)
 		ok, err := addPolicy(role, v.Resource, v.Operate, serviceName)
 		if err != nil {
-			logger.Error("init plugin-admin policy failed:%s", err)
+			logger.Error("init %s policy failed:%s", serviceName, err)
 		}
 		if !ok {
-			logger.Debug("plugin-admin %s permission already exists: %s", v.Operate, v.Resource)
+			logger.Debug("%s %s permission already exists: %s", serviceName, v.Operate, v.Resource)
 		}
 	}
 	return nil
@@ -29,14 +32,31 @@ func AddPluginServicePermission(role string, permissions []common.Permission, se
 
 // 删除插件服务权限
 func DeletePluginServicePermission(permissions []common.Permission, serviceName string) error {
+	PermissionListMap.DeleteDomain(serviceName)
 	for _, v := range permissions {
 		ok, err := G_Enfocer.RemoveFilteredPolicy(1, v.Resource, v.Operate, serviceName)
 		if err != nil {
-			logger.Error("delete plugin policy failed:%s", err)
+			logger.Error("delete %s policy failed:%s", serviceName, err)
 		}
 		if !ok {
-			logger.Debug("delete plugin %s permission failed: %s", v.Operate, v.Resource)
+			logger.Debug("delete %s %s permission failed: %s", serviceName, v.Operate, v.Resource)
 		}
 	}
 	return nil
+}
+
+func initPluginServicePermission() {
+	services := global.GW.GetAllServices()
+	for _, service := range services {
+		var permissions []common.Permission
+		if perms, ok := service["permissions"]; ok {
+			if err := mapstructure.Decode(perms, &permissions); err != nil {
+				logger.Error("decode permission policy failed:%s", err)
+			}
+
+			if err := AddPluginServicePermission("admin", permissions, service["serviceName"].(string)); err != nil {
+				logger.Error("Failed to add permissions for service %s: %v", service["serviceName"].(string), err)
+			}
+		}
+	}
 }
