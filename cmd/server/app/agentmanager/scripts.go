202d45765fa1c19350d38668189c8be3c6246c80
zhanghan <zhanghan@kylinos.cn>
2025-09-16 14:42:44 +0800

    Add new script for plugin SDK and distribute it

--- cmd/server/app/agentmanager/scripts.go
+++ cmd/server/app/agentmanager/scripts.go
@@ -1,21 +1,18 @@
 package agentmanager
 
 import (
-	"encoding/base64"
-
 	"gitee.com/openeuler/PilotGo/pkg/utils"
 	"gitee.com/openeuler/PilotGo/pkg/utils/message/protocol"
 )
 
 // 远程在agent上运行脚本文件
 func (a *Agent) AgentRunScripts(scriptType string, script string, params string) (*utils.CmdResult, error) {
-	encoded_script := base64.StdEncoding.EncodeToString([]byte(script))
 	data := struct {
 		Script     string
 		Params     string
 		ScriptType string
 	}{
-		Script:     encoded_script,
+		Script:     script,
 		ScriptType: scriptType,
 		Params:     params,
 	}
